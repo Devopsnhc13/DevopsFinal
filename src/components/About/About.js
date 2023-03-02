557987e167fba354c973595af93c973665fc3670
Devopsnhc13 <devopsnhc13@gmail.com>
2023-03-02 10:38:15 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,10 +16,10 @@ function About() {
         <p className='about-nike-para'>
           Our mission is what drives us to do everything possible to expand
           human potential. We do that by creating groundbreaking sport
-          innovations, by making our products more sudddstainably, by building a
-          creative and diverseacadcadc global team and by making a positive impact in
-          communities where sacascawe live and work. Based in Beaverton, Oregon, NIKE,
-          Inc. incluascacvfvz des the Nike, Converse, and Jordan brands.
+          innovations, by making our products more sustainably, by building a
+          creative and derivatives global team and by making a positive impact in
+          communities where cascade live and work. Based in Beaverton, Oregon, NIKE,
+          Inc. including this the Nike, Converse, and Jordan brands.
         </p>
       </div>
     </div>
